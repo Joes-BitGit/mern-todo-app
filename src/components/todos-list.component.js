a7aef4085ea274a3ffd6c0e81edc939fa9230636
Joes-BitGit <josephnalmeida@gmail.com>
2020-04-02 19:04:44 -0700

    Completed the Implementation of TodosList Component

--- src/components/todos-list.component.js
+++ src/components/todos-list.component.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-function TodosList() {
-  return (
-    <div>
-      <p>Welcome to Todos List Component!</p>
-    </div>
-  );
-}
+const Todo = props => (
+  <tr>
+    <td>{props.todo.todo_description}</td>
+    <td>{props.todo.todo_responsible}</td>
+    <td>{props.todo.todo_priority}</td>
+    <td>
+      <Link to={"/edit/"+props.todo._id}>Edit</Link>
+    </td>
+  </tr>
+)
 
-export default TodosList;
+export default class TodosList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {todos: []};
+  }
+
+  componentDidMount(){
+    axios.get('http://locasl:4000/todos/')
+      .then(response => {
+        this.setState({todos: response.data});
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+  }
+
+  todoList() {
+    return this.state.todos.map(function(currentTodo, index) {
+      return <Todo todo={currentTodo} key={index} />
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <h3>Todos List</h3>
+        <table className="table table-striped" style={{ marginTop: 20 }}>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Responsible</th>
+              <th>Priority</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.todosList() }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+}
